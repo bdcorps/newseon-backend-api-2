5ce054c3e323fa6f50edebe4eb6f416fabbbebff
Sukhpal Saini <bdcorps@gmail.com>
2019-08-23 13:47:45 +0000

    test gitignore in uploads folder
    separate reload node script to run herok u scheduler

---
+++ reload.js
@@ -0,0 +1,54 @@
+const request = require("request");
+
+//copied from app.js
+//need to be added to a helper file
+const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+var reloadContentAsync = async () => {
+  request("http://newseon-backend-api-2.herokuapp.com/resetv2", function(
+    error,
+    response,
+    body
+  ) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+  });
+  await snooze(5000);
+  console.log("Generating.");
+  request("http://newseon-backend-api-2.herokuapp.com/generatev2", function(
+    error,
+    response,
+    body
+  ) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+  });
+
+  await snooze(5000);
+  console.log("Writing.");
+  request("http://newseon-backend-api-2.herokuapp.com/writesv2", function(
+    error,
+    response,
+    body
+  ) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+  });
+
+  await snooze(5000);
+  console.log("Creating Tracks.");
+  request("http://newseon-backend-api-2.herokuapp.com/tracksv2", function(
+    error,
+    response,
+    body
+  ) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+  });
+};
+
+reloadContentAsync();
