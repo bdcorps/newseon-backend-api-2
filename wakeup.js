2210e9db532655e62ef45f3db20c48cdf55cb837
Sukhpal Saini <bdcorps@users.noreply.github.com>
2019-09-23 09:25:59 -0400

    Remove test from wakeup.js

--- wakeup.js
+++ wakeup.js
@@ -1,12 +1,1 @@
-const { hasConsistentStructure } = require("./utils/helpers");
 
-const article = {
-  source: "",
-  title: "Title",
-  description: "yy",
-  url: "url",
-  urlToImage: "urlToImage",
-  publishedAt: "date"
-};
-
-console.log(hasConsistentStructure(article));
